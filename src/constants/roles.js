3940cc28725835ea1e0813af10388a2a7a2080f0
nayan9617 <nayanp4925@gmail.com>
2026-04-01 23:08:38 +0530

    feat: add shared constants and utility helpers

---
+++ src/constants/roles.js
@@ -0,0 +1,15 @@
+const ROLES = {
+  ADMIN: "admin",
+  ANALYST: "analyst",
+  VIEWER: "viewer",
+};
+
+const USER_STATUSES = {
+  ACTIVE: "active",
+  INACTIVE: "inactive",
+};
+
+module.exports = {
+  ROLES,
+  USER_STATUSES,
+};
---
+++ src/utils/apiResponse.js
@@ -0,0 +1,9 @@
+const sendSuccess = (res, data, message = "Success", statusCode = 200) => {
+  return res.status(statusCode).json({
+    success: true,
+    message,
+    data,
+  });
+};
+
+module.exports = { sendSuccess };
